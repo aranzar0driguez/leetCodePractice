18771e526f1134e96ecef7072ac72d8ecac68791
Aranza Rodriguez <rodriguez.aranza.9801@gmail.com>
2025-01-25 23:57:10 -0600

    solved Q26

---
+++ removeduplicatesarray.py
@@ -0,0 +1,18 @@
+#Creates two pointers, one that tracks the num of unique values and 
+#another one that tracks current element in array to compare uniqueness 
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        l = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[l] = nums[i]
+                l += 1
+
+        return l
